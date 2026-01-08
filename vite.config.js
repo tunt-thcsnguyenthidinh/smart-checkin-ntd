f500160328e93600a6edeccee73bfdf678927ab2
tunt-thcsnguyenthidinh <tunt_thcsnguyenthidinh_longdat@bariavungtau.edu.vn>
2026-01-08 22:27:30 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Quan trọng: Nâng target lên es2020 để hỗ trợ import.meta.env không bị lỗi warning
+    target: 'es2020', 
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true, // Tự động mở trình duyệt khi chạy npm run dev
+  }
 })
